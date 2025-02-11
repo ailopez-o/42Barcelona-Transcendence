8ed32a096ef06d7ffb3b9c86d3584bb349ac2312
ailopez-o <aitorlpzaudikana@gmail.com>
2025-02-11 15:04:02 +0000

    gestion de invitacion de partidas

--- standalone/transcendance/app/urls.py
+++ standalone/transcendance/app/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('game/new/', views.new_game_view, name='new_game'),
     path('game/<int:game_id>/', views.game_detail_view, name='game_detail'),
+    path('game/<int:game_id>/accept/', views.accept_game_view, name='accept_game'),
+    path('game/<int:game_id>/reject/', views.reject_game_view, name='reject_game'),
     path('tournament/new/', views.new_tournament_view, name='new_tournament'),
     path('tournament/<int:tournament_id>/', views.tournament_detail_view, name='tournament_detail'),
 ]
