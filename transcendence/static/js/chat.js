5034875fb486c80aa56270f2b5b9d636c04dce45
Aitor Lopez de Audícana <aitorlpzaudikana@gmail.com>
2025-02-22 18:43:55 +0100

    current user handle

--- transcendence/static/js/chat.js
+++ transcendence/static/js/chat.js
@@ -3,6 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
     // Puedes adaptar la URL según cómo hayas configurado el routing en tu proyecto.
     const chatSocket = new WebSocket(`ws://${window.location.host}/ws/chat/`);
 
+    // Obtener el bloque JSON y parsearlo
+    const playerDataElement = document.getElementById("player-data");
+    const playerData = JSON.parse(playerDataElement.textContent);
+    
+    // Usar la propiedad "current" para obtener el objeto correspondiente
+    const currentPlayer = playerData[playerData.current];
+
     // Elementos del DOM
     const chatContainer = document.getElementById("chat-container");
     const chatForm = document.getElementById("chat-form");
@@ -31,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (message !== "") {
             const payload = {
                 type: "chat",
-                username: "{{ request.user.username }}",  // Considera pasar este dato desde el template o usando un data-attribute
+                username: "{{ currentPlayer.username }}",
                 message: message
             };
             chatSocket.send(JSON.stringify(payload));
