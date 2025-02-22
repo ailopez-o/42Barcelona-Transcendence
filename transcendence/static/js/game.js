d385f5424dbeb00db0fdef8ab5e72476497108ce
albagarc <alba.garcia@antaiventures.com>
2025-02-22 21:03:47 +0100

    update state

--- transcendence/static/js/game.js
+++ transcendence/static/js/game.js
@@ -1,3 +1,32 @@
+class Ball {
+    constructor(x, y, radius, speed) {
+      this.radius = radius;
+      this.position = {
+        x: x,
+        y: y
+      };
+      this.velocity = {
+        x: speed.x,
+        y: speed.y
+      };
+    }
+  }
+
+class Paddle {
+    constructor(x, y, width, height, speed) {
+      this.width = width;
+      this.height = height;
+      this.position = {
+        x: x,
+        y: y
+      };
+      this.velocity = {
+        y: 0
+      };
+      this.maxSpeed = speed;
+    }
+  }
+
 document.addEventListener("DOMContentLoaded", function() {
     const gameId = document.getElementById("game-container").dataset.gameId;
     const username = document.getElementById("game-container").dataset.username;
@@ -9,10 +38,31 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("Conectado al WebSocket");
     };
 
+    // Crear objeto para el estado del juego
+    const gameState = {
+        ball: {
+            x: 400,
+            y: 200,
+            radius: 10
+        },
+        paddles: {
+            left: { y: 150 },
+            right: { y: 150 }
+        }
+    };
+
     socket.onmessage = function(event) {
         console.log("Mensaje recibido:", event.data);
         const data = JSON.parse(event.data);
         document.getElementById("status-message").innerText = data.message;
+        
+        // Actualizar el estado del juego con los datos recibidos
+        if (data.game_state) {
+            gameState.ball = data.game_state.ball;
+            gameState.paddles = data.game_state.paddles;
+            // Dibujar el juego con los nuevos datos
+            drawGame();
+        }
     };
 
     socket.onclose = function(event) {
@@ -31,10 +81,12 @@ document.addEventListener("DOMContentLoaded", function() {
         ctx.fillStyle = "black";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+        // Dibujar las palas usando el estado del juego
         ctx.fillStyle = "red";
-        ctx.fillRect(20, 150, 10, 100); // Pala izquierda
-        ctx.fillRect(770, 150, 10, 100); // Pala derecha
+        ctx.fillRect(20, gameState.paddles.left.y, 10, 100); // Pala izquierda
+        ctx.fillRect(770, gameState.paddles.right.y, 10, 100); // Pala derecha
 
+        // Dibujar la línea central
         ctx.strokeStyle = 'white';
         ctx.setLineDash([10, 15]);
         ctx.lineWidth = 8;
@@ -43,9 +95,9 @@ document.addEventListener("DOMContentLoaded", function() {
         ctx.lineTo(canvas.width / 2, canvas.height);
         ctx.stroke();
 
-
+        // Dibujar la pelota usando el estado del juego
         ctx.beginPath();
-        ctx.arc(400, 200, 10, 0, Math.PI * 2);
+        ctx.arc(gameState.ball.x, gameState.ball.y, gameState.ball.radius, 0, Math.PI * 2);
         ctx.fill();
     }
 
