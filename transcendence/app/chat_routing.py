5758a49bfe06b1b76fc5e3c3f608215a2f0a2b74
ailopez-o <aitorlpzaudikana@gmai.com>
2025-02-23 22:57:47 +0100

    Chat global y chat por partidas

--- transcendence/app/chat_routing.py
+++ transcendence/app/chat_routing.py
@@ -3,6 +3,8 @@ from django.urls import re_path
 from .chat_consumer import ChatConsumer
 
 websocket_urlpatterns = [
-    # Aquí definimos la URL para el chat.
+    # Ruta para chat global sin parámetro
     re_path(r'^ws/chat/$', ChatConsumer.as_asgi()),
+    # Ruta para chat de una partida específica, que recibe un game_id
+    re_path(r'^ws/chat/(?P<game_id>\d+)/$', ChatConsumer.as_asgi()),
 ]
