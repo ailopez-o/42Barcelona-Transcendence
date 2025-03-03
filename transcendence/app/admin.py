c08540a5290660fadde489380d0741d477e8fee6
Aitor López de Audicana <aitor.lopez@igrid-td.com>
2025-03-03 11:23:13 +0100

    refactor and clean models

--- transcendence/app/admin.py
+++ transcendence/app/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from .models import User, Game, GameEvent, Tournament, TournamentGame, RemoteGameSession, GameResult
+from .models import User, Game, Tournament, TournamentGame, GameResult
 
 admin.site.register(User)
 admin.site.register(Game)
-admin.site.register(GameEvent)
 admin.site.register(Tournament)
 admin.site.register(TournamentGame)
-admin.site.register(RemoteGameSession)
 admin.site.register(GameResult)
 
 
