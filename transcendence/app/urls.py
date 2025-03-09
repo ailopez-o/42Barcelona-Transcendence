aec21b38089c8ae76b1310e5dab33405cc9ecec2
Aitor Lopez de Audícana <aitorlpzaudikana@gmail.com>
2025-03-09 17:27:54 +0100

    avatar url better logic register template nginx serve media folder

--- transcendence/app/urls.py
+++ transcendence/app/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='home'),
+    path('', views.home_view, name='home'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
