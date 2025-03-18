059d08f82ea03f371a1222dd47486b3e3163b38f
ailopez- <aitorlpzaudikana@gmail.com>
2025-03-18 10:13:41 +0100

    asgi routing update

--- transcendence/mysite/asgi.py
+++ transcendence/mysite/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter 
 from app.consumers.game_routing import websocket_urlpatterns as game_ws_urlpatterns
@@ -18,6 +19,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 combined_websocket_urlpatterns = game_ws_urlpatterns + chat_ws_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  # Maneja las peticiones HTTP
-    "websocket": URLRouter(combined_websocket_urlpatterns),  # Maneja las conexiones WebSocket
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(combined_websocket_urlpatterns)
+    ),
 })
